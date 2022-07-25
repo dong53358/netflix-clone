0b9066e65694be4a13c32d77c9cb6a0a8a213b3f
dong53358 <dong53358@naver.com>
2022-07-25 18:42:44 +0900

    Header part

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 import styled from "styled-components";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Router/Home";
+import Tv from "./Router/Tv";
+import Search from "./Router/Search";
+import Header from "./Components/Header";
 
 function App() {
-  return null;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/tv" element={<Tv />}></Route>
+        <Route path="/search" element={<Search />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/Router/Tv.tsx
@@ -0,0 +1,5 @@
+function Tv() {
+  return <div style={{ height: "200vh" }}></div>;
+}
+
+export default Tv;
